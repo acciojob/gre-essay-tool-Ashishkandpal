98b4b25bace9d41043e14454afc020a5020621ad
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-03-01 13:29:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ let wordsCountArray = [];
 evaluate.addEventListener("keydown", function (e) {
   wordsCountArray = evaluate.value.split(" ");
   wordCount.textContent = wordsCountArray.length;
+  //   console.log(wordsCountArray);
+  if (e.key == "Backspace" && evaluate.value.length == 1) {
+    wordCount.textContent = 0;
+  }
+  console.log(e.key);
 });
