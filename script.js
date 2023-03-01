45596a6cdbf25841983ed48736af8394fe7bf9fa
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-03-01 13:04:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+const evaluate = document.querySelector("#evaluatedText");
+const wordCount = document.querySelector("#wordCount");
+let wordsCountArray = [];
+
+evaluate.addEventListener("keydown", function (e) {
+  wordsCountArray = evaluate.value.split(" ");
+  wordCount.textContent = wordsCountArray.length;
+});
